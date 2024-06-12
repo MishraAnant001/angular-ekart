0b56041c355120180e19f03bf82bc95e24711f48
ANANT MISHRA <anantmanojmishra00@gmail.com>
2024-06-12 19:41:45 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,8 @@ import { TopHeaderComponent } from './components/top-header/top-header.component
 import { TopMenuComponent } from './components/header/top-menu/top-menu.component';
 import { MainMenuComponent } from './components/header/main-menu/main-menu.component';
 import { ProductListComponent } from './components/product-list/product-list.component';
+import { SearchComponent } from './components/product-list/search/search.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -16,9 +18,11 @@ import { ProductListComponent } from './components/product-list/product-list.com
     TopMenuComponent,
     MainMenuComponent,
     ProductListComponent,
+    SearchComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
